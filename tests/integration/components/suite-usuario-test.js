61519e3ebc44b9dc43979d6eb012f9748c2837ff
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-01-02 18:18:14 -0300

    Simplificando el tests de suite-usuario.

--- tests/integration/components/suite-usuario-test.js
+++ tests/integration/components/suite-usuario-test.js
@@ -12,14 +12,5 @@ test('it renders', function(assert) {
 
   this.render(hbs`{{suite-usuario}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#suite-usuario}}
-      template block text
-    {{/suite-usuario}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text());
 });
