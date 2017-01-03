f733f56458a7ccceb7b046f97e89f41dc908ab3a
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-01-03 00:48:56 -0300

    Agregando mensajes para indicar que algunos botones no están implementados.

--- app/components/suite-usuario.js
+++ app/components/suite-usuario.js
@@ -5,11 +5,21 @@ export default Ember.Component.extend({
   classNames: ['item', 'suite-usuario'],
 
   didInsertElement() {
-    this.$()
-      .popup({
+    this.$().popup({
         popup: '.popup',
         on: 'click',
         closeable: true,
-      });
+    });
+  },
+
+  actions: {
+    abrirPreferencias() {
+      alert("Esta funcionalidad aún no está implementada.");
+      this.$().popup('hide');
+    },
+    cerrarLaSesion() {
+      alert("Esta funcionalidad aún no está implementada.");
+      this.$().popup('hide');
+    }
   }
 });
