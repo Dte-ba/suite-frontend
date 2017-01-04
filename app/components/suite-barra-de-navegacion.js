f0b1210af26927291e975734646b66fdec77971a
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-01-04 01:01:56 -0300

    Reparando el menú sidebar en mobile.

--- app/components/suite-barra-de-navegacion.js
+++ app/components/suite-barra-de-navegacion.js
@@ -2,10 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ["ui", "fixed", "menu", "inverted"],
-
   actions: {
-    mostrarMenuLateral() {
-      this.$('.ui.sidebar').sidebar('toggle');
+    toggle: function(id) {
+      $(`#${id}`).sidebar('toggle');
     }
   }
 });
