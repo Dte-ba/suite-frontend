0ab12005e659b5571007d07a3e9dae3cd6015faf
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-01-12 23:15:19 -0300

    Haciendo que el menú se oculte al hacer click en mobile y evitando que la barra de scroll mueva el contenido hacia la izquierda de la pantalla.

--- app/components/suite-lista-de-rutas.js
+++ app/components/suite-lista-de-rutas.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: ''
+  tagName: '',
+  didInsertElement() {
+    $("#sub-sidebar a").off('click');
+
+    $("#sub-sidebar a").on('click', function(e) {
+      $('#sub-sidebar').sidebar('hide');
+    });
+  }
 });
