e19f6196ee95f93781c88b721df67259cc230f2f
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-03-05 02:14:33 -0300

    Agregando url para acceder al backend de producción.

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,7 @@ module.exports = function(environment) {
     environment: environment,
     rootURL: '/',
     locationType: 'hash',
+    API_URL: 'http://testing-suite-admin.dtelab.com.ar',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
