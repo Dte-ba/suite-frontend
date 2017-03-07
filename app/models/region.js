c223b215fb3c6544620bdae428d6689f6977bc34
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-03-06 23:37:00 -0300

    Adaptando el modelo de región ahora que el campo 'numero' ahora no es más un string.

--- app/models/region.js
+++ app/models/region.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  numero: DS.attr('string'),
+  numero: DS.attr('number'),
   municipios: DS.hasMany('municipio'),
 
   municipiosComoCadena: Ember.computed('municipios', function() {
