29a9e9a88fa81949be87974e822cf61593edea03
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-05-26 09:32:18 -0300

    Llevando la configuración de plurales a un inicializador para que funcione correctamente dentro de mirage.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -6,6 +6,3 @@ export default DS.JSONAPIAdapter.extend({
   host: ENV.API_URL,
   namespace: 'api'
 });
-
-Ember.Inflector.inflector.irregular('region', 'regiones');
-Ember.Inflector.inflector.irregular('perfil', 'perfiles');
---
+++ app/initializers/configure-inflector.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+import config from '../config/environment';
+
+let initializer = {
+  name: 'configure-inflector',
+
+  initialize: function() {
+    const inflector = Ember.Inflector.inflector;
+
+    inflector.irregular('region', 'regiones');
+    inflector.irregular('perfil', 'perfiles');
+
+  }
+};
+
+if (config.environment !== 'production') {
+  initializer.before = 'ember-cli-mirage';
+}
+
+export default initializer;
