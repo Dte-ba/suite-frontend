5899b7f7dfec344e068341ba8dc011571d9185d8
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-06-10 23:15:28 -0300

    Arreglo menor en el tests de pruebas.

--- tests/acceptance/puede-ingresar-apruebas-ui-test.js
+++ tests/acceptance/puede-ingresar-apruebas-ui-test.js
@@ -16,9 +16,10 @@ function clickSobreElTexto(texto) {
 }
 
 test('visiting /app/ui', function(assert) {
-  visit('/app/ui');
+  visit('/login');
 
   andThen(function() {
+    assert.equal(currentURL(), '/login');
     clickSobreElTexto("Ingresar como dte");
   });
 
