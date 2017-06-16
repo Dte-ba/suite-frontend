8d9a7c5433815f389a9d61030202d29a4980f5ce
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-06-16 10:01:54 -0300

    Arreglo menor.

--- app/components/full-calendar.js
+++ app/components/full-calendar.js
@@ -4,14 +4,18 @@ import Ember from "ember";
 export default Component.extend({
   didInsertElement() {
     this._super();
-    this.forceResize();
+    this.sincronizar();
   },
 
   alternarFinesDeSemana: Ember.observer("mostrarFinesDeSemana", function() {
+    this.sincronizar();
+  }),
+
+  sincronizar() {
     const fc = this.$();
     fc.fullCalendar("option", "weekends", this.get("mostrarFinesDeSemana"));
     this.forceResize();
-  }),
+  },
 
   forceResize() {
     $(window).resize();
