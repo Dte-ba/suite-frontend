f1c03feb3bff7d9517478107d6a2bf001d5a0087
Ivan Hoffmann <digital.h@gmail.com>
2017-06-23 14:39:21 -0300

    Iniciando vista de detalle de escuela con datos básicos.

--- app/routes/app/escuelas/detalle.js
+++ app/routes/app/escuelas/detalle.js
@@ -1,4 +1,90 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Route.extend({
+  breadCrumb: { title: "Detalle de escuela" },
+
+  afterModel(model) {
+    model.set("filaPiso", [
+      {
+        titulo: "Marca",
+        id: ""
+      },
+      {
+        titulo: "Serie",
+        id: ""
+      },
+      {
+        titulo: "UPS",
+        id: ""
+      },
+      {
+        titulo: "Rack",
+        id: ""
+      },
+      {
+        titulo: "Estado",
+        id: ""
+      }
+    ]);
+
+    model.set("filaInformacion", [
+      {
+        titulo: "CUE",
+        id: "cue"
+      },
+      {
+        titulo: "Nro. Provincial",
+        id: ""
+      },
+      {
+        titulo: "Nivel",
+        id: "nivel.nombre"
+      },
+      {
+        titulo: "Modalidad",
+        id: ""
+      },
+      {
+        titulo: "Programas",
+        id: "programasComoCadena"
+      },
+      {
+        titulo: "CUE anteriores",
+        id: ""
+      },
+      {
+        titulo: "Anexo",
+        id: ""
+      }
+    ]);
+
+    model.set("filaDatosDeContacto", [
+      {
+        titulo: "Teléfono",
+        id: "telefono"
+      },
+      {
+        titulo: "Email",
+        id: "email"
+      },
+      {
+        titulo: "Dirección",
+        id: "direccion"
+      },
+      {
+        titulo: "Localidad",
+        id: "localidad.nombre"
+      },
+      {
+        titulo: "Distrito",
+        id: "localidad.distrito.nombre"
+      },
+      {
+        titulo: "Región",
+        id: "localidad.distrito.region.numero"
+      }
+    ]);
+
+    return model;
+  }
 });
