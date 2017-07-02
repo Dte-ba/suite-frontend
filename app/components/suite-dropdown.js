278ca6bdea2347aabe2a693bb65f9e3eb31b96da
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-07-02 02:55:45 -0300

    Implementando selección dropdown usando powerselect.

---
+++ app/components/suite-dropdown.js
@@ -0,0 +1,9 @@
+import Ember from "ember";
+
+export default Ember.Component.extend({
+  actions: {
+    cuandoCambia(opcion) {
+      this.cuandoSelecciona(opcion);
+    }
+  }
+});
--- app/routes/app/escuelas/edicion.js
+++ app/routes/app/escuelas/edicion.js
@@ -1,10 +1,16 @@
 import Ember from "ember";
 
 export default Ember.Route.extend({
+  afterModel(model) {
+    model.set(
+      "tiposDeFinanciamiento",
+      this.store.findAll("tipoDeFinanciamiento")
+    );
+  },
   actions: {
     guardar(modelo) {
       modelo.save().then(() => {
-        this.transitionTo("app.escuelas.detalle", modelo);
+        this.transitionTo("app.escuelas.edicion", modelo);
       });
     }
   }
