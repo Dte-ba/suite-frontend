25d954003571283f3f83ef59920b3e94316fb8ba
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-07-02 02:57:46 -0300

    Arreglo menor.

--- app/routes/app/escuelas/edicion.js
+++ app/routes/app/escuelas/edicion.js
@@ -10,7 +10,7 @@ export default Ember.Route.extend({
   actions: {
     guardar(modelo) {
       modelo.save().then(() => {
-        this.transitionTo("app.escuelas.edicion", modelo);
+        this.transitionTo("app.escuelas.detalle", modelo);
       });
     }
   }
