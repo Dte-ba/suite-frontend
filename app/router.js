67bc51efce22298112da65ae01db753553b6c9d5
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-07-04 10:38:08 -0300

    Creando la ruta que permite listar todos los eventos en una tabla.

--- app/router.js
+++ app/router.js
@@ -27,6 +27,7 @@ Router.map(function() {
     this.route("agenda", function() {
       this.route("crear");
       this.route("editar", { path: "editar/:id" });
+      this.route('lista');
     });
 
     this.route("regiones", function() {
