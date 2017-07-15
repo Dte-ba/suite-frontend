c2721257aa265fda847ff5264c5c37b503b51561
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-07-14 21:59:47 -0300

    Mejorando la forma de mostrar relaciones asincrónicas con spinners.

--- app/components/suite-promesa.js
+++ app/components/suite-promesa.js
@@ -1,7 +1,3 @@
 import Ember from "ember";
 
-export default Ember.Component.extend({
-  cargando: Ember.computed("valor", function() {
-    return this.get("valor") === undefined;
-  })
-});
+export default Ember.Component.extend({});
