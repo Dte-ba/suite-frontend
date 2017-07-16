e84fe15832db32a7ad3c7b8651c9b5aa78d02cd0
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-07-16 17:46:10 -0300

    Implementando power-select en la creación de eventos para vincular un responsable.

---
+++ app/components/power-select.js
@@ -0,0 +1,7 @@
+import EmberPowerSelect from "ember-power-select/components/power-select";
+
+export default EmberPowerSelect.extend({
+  searchMessage: "Ingrese algún texto para buscar",
+  noMatchesMessage: "No hay resultados",
+  loadingMessage: "Buscando ..."
+});
--- app/routes/app/agenda/crear.js
+++ app/routes/app/agenda/crear.js
@@ -1,5 +1,7 @@
 import Ember from "ember";
 
+import { task, timeout } from "ember-concurrency";
+
 export default Ember.Route.extend({
   model() {
     let hoy = "2017-10-10T20:00";
@@ -8,6 +10,15 @@ export default Ember.Route.extend({
       fechafin: hoy,
       titulo: "demo 123123"
     });
+  },
+
+  buscarPersonas: task(function*(term) {
+    yield timeout(200);
+    return this.store.query("perfil", { search: term });
+  }),
+
+  afterModel(model) {
+    model.set("buscarPersonas", this.get("buscarPersonas"));
   },
 
   actions: {
