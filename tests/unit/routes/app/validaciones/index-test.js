a8639a961cf3e266d0cafa6c64d39352105a04b1
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-08-15 17:00:42 -0300

    Agregando dependencia a los tests de unidad.

--- tests/unit/routes/app/validaciones/index-test.js
+++ tests/unit/routes/app/validaciones/index-test.js
@@ -2,7 +2,7 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:app/validaciones/index', 'Unit | Route | app/validaciones/index', {
   // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ["service:ajax"]
 });
 
 test('it exists', function(assert) {
