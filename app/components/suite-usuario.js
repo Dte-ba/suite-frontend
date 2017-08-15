84d22bd041a3090fa41c29fcd45c9c6e6e5a619d
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-08-15 01:34:59 -0300

    Reparando el botón para cerrar sesión.

--- app/components/suite-usuario.js
+++ app/components/suite-usuario.js
@@ -3,8 +3,7 @@ import Ember from "ember";
 export default Ember.Component.extend({
   session: Ember.inject.service(),
   perfil: Ember.inject.service(),
-  tagName: "a",
-  classNames: ["ui", "dropdown", "icon", "item"],
+  tagName: "",
 
   actions: {
     invalidateSession() {
