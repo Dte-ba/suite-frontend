bab72ab6d783bf637ca338fad4a4eede3e4bab7b
digitalh <digital.h@gmail.com>
2017-08-15 17:20:17 -0300

    Implementando estadisticas de personas/index

--- app/routes/app/personas/index.js
+++ app/routes/app/personas/index.js
@@ -1,15 +1,25 @@
 import Ember from "ember";
 import { task } from "ember-concurrency";
+import ENV from "suite-frontend/config/environment";
 
 export default Ember.Route.extend({
+  ajax: Ember.inject.service(),
+
   obtenerPersonas: task(function*(query) {
     let data = yield this.store.query("perfil", query);
     let meta = data.get("meta");
     return { data, meta };
   }).drop(),
 
+  obtenerEstadisticas: task(function*() {
+    let url = ENV.API_URL + "/api/perfiles/estadistica";
+    let resultado = yield this.get("ajax").request(url);
+    return resultado;
+  }).drop(),
+
   model() {
     return Ember.RSVP.hash({
+      estadisticas: this.get("obtenerEstadisticas").perform({}),
       tareaPersonas: this.get("obtenerPersonas"),
       columnas: [
         {
