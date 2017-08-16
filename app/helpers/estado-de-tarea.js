112a20bb0b82fa1782ac270a3891fdc4b19cdfc5
Iván Hoffmann <digital.h@gmail.com>
2017-08-16 16:47:21 -0300

    Ajustes en detalle de tareas. Colores de estado y prioridad. If para comentarios.

--- app/helpers/estado-de-tarea.js
+++ app/helpers/estado-de-tarea.js
@@ -3,13 +3,13 @@ import Ember from 'ember';
 export function estadoDeTarea(params/*, hash*/) {
   let estado = params[0];
   if (estado === "Cerrado" ) {
-    estado = '<i class="ui checkmark icon"></i>' + estado;
+    estado = '<i class="ui green checkmark icon"></i>' + estado;
   } else if (estado === "En Espera") {
-    estado = '<i class="ui wait empty icon"></i>' + estado;
+    estado = '<i class="ui yellow wait empty icon"></i>' + estado;
   } else if (estado === "En Progreso") {
-    estado = '<i class="ui hourglass half icon"></i>' + estado;
+    estado = '<i class="ui yellow hourglass half icon"></i>' + estado;
   } else if (estado === "Abierto") {
-    estado = '<i class="ui hourglass empty down icon"></i>' + estado;
+    estado = '<i class="ui red hourglass empty down icon"></i>' + estado;
   }
   return Ember.String.htmlSafe(estado);
 }
--- app/helpers/prioridad-de-tarea.js
+++ app/helpers/prioridad-de-tarea.js
@@ -3,11 +3,11 @@ import Ember from 'ember';
 export function prioridadDeTarea(params/*, hash*/) {
   let prioridad = params[0];
   if (prioridad === "Alta" ) {
-    prioridad = '<i class="ui long arrow up icon"></i>' + prioridad;
+    prioridad = '<i class="ui red long arrow up icon"></i>' + prioridad;
   } else if (prioridad === "Media") {
-    prioridad = '<i class="ui long arrow right icon"></i>' + prioridad;
+    prioridad = '<i class="ui yellow long arrow right icon"></i>' + prioridad;
   } else if (prioridad === "Baja") {
-    prioridad = '<i class="ui long arrow down icon"></i>' + prioridad;
+    prioridad = '<i class="ui green long arrow down icon"></i>' + prioridad;
   }
   return Ember.String.htmlSafe(prioridad);
 }
