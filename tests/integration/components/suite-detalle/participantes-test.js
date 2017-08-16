91feaf77868b9fff55e1dd452a6215e88418ad90
Iván Hoffmann <digital.h@gmail.com>
2017-08-16 13:47:06 -0300

    Fix provisorio de test de componente suite-detalle/participantes

--- tests/integration/components/suite-detalle/participantes-test.js
+++ tests/integration/components/suite-detalle/participantes-test.js
@@ -5,21 +5,8 @@ moduleForComponent('suite-detalle/participantes', 'Integration | Component | sui
   integration: true
 });
 
-test('it renders', function(assert) {
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
+test("it renders", function(assert) {
   this.render(hbs`{{suite-detalle/participantes}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#suite-detalle/participantes}}
-      template block text
-    {{/suite-detalle/participantes}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), "Sin acompañantes");
 });
