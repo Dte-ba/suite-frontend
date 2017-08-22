02dda3bccec188187b0f185e741fa19135e7c8ef
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-08-22 00:19:14 -0300

    Reparando los bugs que ocasionaban que el selector de fechas y horas en nuevos eventos no guarden el valor al cerrar el popup.

--- app/components/suite-campo/fecha.js
+++ app/components/suite-campo/fecha.js
@@ -2,11 +2,16 @@ import Ember from "ember";
 
 export default Ember.Component.extend({
   formato: "YYYY-MM-DD",
+  valorAnterior: null,
 
   actions: {
     cuandoCambiaFecha(valor) {
       let valorConvertido = moment(valor).format(this.get("formato"));
-      this.get("cuandoCambia")(valorConvertido);
+
+      if (this.get("valorAnterior") != valorConvertido) {
+        this.get("cuandoCambia")(valorConvertido);
+        this.set("valorAnterior", valorConvertido);
+      }
     }
   }
 });
--- app/components/suite-campo/hora.js
+++ app/components/suite-campo/hora.js
@@ -2,6 +2,7 @@ import Ember from "ember";
 
 export default Ember.Component.extend({
   contador: 0,
+  valorAnterior: null,
 
   horaInicial: Ember.computed("hora", function() {
     if (this.get("hora")) {
@@ -14,11 +15,9 @@ export default Ember.Component.extend({
       this.incrementProperty("contador");
       let valorConvertido = moment(valor).format("HH:mm:00");
 
-      /* Evita llamar dos veces al setter cuando permanece abierto el
-         popup del selector de hora. */
-      if (this.get("contador") == 2) {
-        this.set("contador", 0);
+      if (this.get("valorAnterior") != valorConvertido) {
         this.get("cuandoCambia")(valorConvertido);
+        this.set("valorAnterior", valorConvertido);
       }
     }
   }
