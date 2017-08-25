41d0fe9d0281936cd8641fc3baf197938ab65655
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-08-25 12:35:06 -0300

    Reparando el formulario de tarea.

--- app/models/tarea.js
+++ app/models/tarea.js
@@ -1,24 +1,25 @@
-import DS from 'ember-data';
+import DS from "ember-data";
 import {
   validatePresence,
   validateLength
 } from "ember-changeset-validations/validators";
 
 export default DS.Model.extend({
-  titulo: DS.attr('string'),
-  fechaDeAlta: DS.attr('string'),
-  autor: DS.belongsTo('perfil'),
-  responsable: DS.belongsTo('perfil'),
-  descripcion: DS.attr('string'),
-  motivoDeTarea: DS.belongsTo('motivoDeTarea'),
-  estadoDeTarea: DS.belongsTo('estadoDeTarea'),
-  prioridadDeTarea: DS.belongsTo('prioridadDeTarea'),
-  escuela: DS.belongsTo('escuela'),
-  comentariosDeTarea: DS.hasMany('comentarioDeTarea'),
+  titulo: DS.attr("string"),
+  fechaDeAlta: DS.attr("string"),
+  autor: DS.belongsTo("perfil"),
+  responsable: DS.belongsTo("perfil"),
+  descripcion: DS.attr("string"),
+  motivoDeTarea: DS.belongsTo("motivoDeTarea"),
+  estadoDeTarea: DS.belongsTo("estadoDeTarea"),
+  prioridadDeTarea: DS.belongsTo("prioridadDeTarea"),
+  escuela: DS.belongsTo("escuela"),
+  comentariosDeTarea: DS.hasMany("comentarioDeTarea"),
 
-
-    validaciones: {
-      titulo: [validatePresence(true), validateLength({ min: 2 })]
-    }
-
+  validaciones: {
+    titulo: [validatePresence(true), validateLength({ min: 2 })],
+    escuela: [validatePresence(true)],
+    descripcion: [validatePresence(true)],
+    motivoDeTarea: [validatePresence(true)]
+  }
 });
--- app/routes/app/tareas/crear.js
+++ app/routes/app/tareas/crear.js
@@ -1,12 +1,11 @@
 import Ember from "ember";
-import { task } from "ember-concurrency";
 
 export default Ember.Route.extend({
   perfil: Ember.inject.service(),
 
   model() {
     return new Ember.RSVP.hash({
-      titulo: "titulo por omisión",
+      titulo: "",
       estadoDeTarea: this.store.findRecord("estadoDeTarea", 1),
       autor: this.get("perfil.miPerfil"),
       responsable: this.get("perfil.miPerfil"),
@@ -15,7 +14,7 @@ export default Ember.Route.extend({
     }).then(valoresPorOmision => {
       let opciones = {
         titulo: valoresPorOmision.titulo,
-        fechaDeAlta: moment(new Date()).format('YYYY-MM-DD'),
+        fechaDeAlta: moment(new Date()).format("YYYY-MM-DD"),
         autor: valoresPorOmision.autor,
         estadoDeTarea: valoresPorOmision.estadoDeTarea,
         responsable: valoresPorOmision.responsable,
@@ -27,28 +26,20 @@ export default Ember.Route.extend({
   },
 
   afterModel(model) {
-    model.set("tareaGuardar", this.tareaGuardar);
     model.set("opciones", {
       prioridadesDeTareas: this.store.findAll("prioridadDeTarea"),
       motivosDeTarea: this.store.findAll("motivoDeTarea")
     });
   },
 
-  tareaGuardar: task(function*(modelo) {
-    try {
-      let resultado = yield modelo.save();
-      this.transitionTo("app.tareas.index");
-      return resultado;
-    } catch (e) {
-      throw new Error("Ha ocurrido un error del lado del servidor");
-    }
-  }).drop(),
-
   actions: {
     willTransition: function() {
       if (this.currentModel.get("isNew")) {
         this.get("currentModel").deleteRecord();
       }
+    },
+    regresar() {
+      return this.transitionTo("app.tareas.index");
     },
     cancelar() {
       return this.transitionTo("app.tareas.index");
