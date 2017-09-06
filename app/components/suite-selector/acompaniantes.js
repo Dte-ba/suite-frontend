8803bdd6ca765f67f98e03e98076f1780777c876
Iván Hoffmann <digital.h@gmail.com>
2017-09-06 16:14:55 -0300

    Implementando selector de acompañantes para eventos

---
+++ app/components/suite-selector/acompaniantes.js
@@ -0,0 +1,24 @@
+import Ember from "ember";
+import { task, timeout } from "ember-concurrency";
+
+export default Ember.Component.extend({
+  store: Ember.inject.service(),
+
+  buscarPersonas: task(function*(term) {
+    yield timeout(200);
+    let query = { search: term };
+
+    if (this.get("region")) {
+      query.region__numero = this.get("region");
+    }
+
+    return this.get("store").query("perfil", query);
+  }),
+
+  actions: {
+    cuandoSeleccionaPerfil(perfil) {
+      this.set("valor", perfil);
+      this.get("cuandoSelecciona")(perfil);
+    }
+  }
+});
