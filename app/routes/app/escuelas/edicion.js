00c1ab569f070b0082d8ad9127bafee7027cb228
Iván Hoffmann <digital.h@gmail.com>
2017-09-22 14:52:56 -0300

    Arreglando edicion de escuela, utilizando nueva versión de formulario

--- app/routes/app/escuelas/edicion.js
+++ app/routes/app/escuelas/edicion.js
@@ -2,23 +2,26 @@ import Ember from "ember";
 
 export default Ember.Route.extend({
   afterModel(model) {
-    model.set(
-      "tiposDeFinanciamiento",
-      this.store.findAll("tipoDeFinanciamiento")
-    );
+    model.set("opciones", {
+      niveles: this.store.findAll("nivel"),
+      areas: this.store.findAll("area"),
+      modalidades: this.store.findAll("modalidad"),
+      tiposDeFinanciamiento: this.store.findAll("tipo-de-financiamiento"),
+      tiposDeGestion: this.store.findAll("tipo-de-gestion"),
+      localidades: this.store.findAll("localidad")
+    });
 
-    model.set("localidades", this.store.findAll("localidad"));
-
-    if (model.get('conformada')) {
+    if (model.get("conformada")) {
       // TODO: Avisar que no puede editar una escuela conformada.
       this.transitionTo("app.escuelas.detalle", model);
     }
   },
   actions: {
-    guardar(modelo) {
-      modelo.save().then(() => {
-        this.transitionTo("app.escuelas.detalle", modelo);
-      });
+    regresar(model) {
+      return this.transitionTo("app.escuelas.detalle", model);
+    },
+    cancelar(model) {
+      return this.transitionTo("app.escuelas.detalle", model);
     }
   }
 });
