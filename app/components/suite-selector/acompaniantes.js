6b0beb33a533b8be1af08b1ab071451f53177cda
Iván Hoffmann <digital.h@gmail.com>
2017-09-23 16:13:57 -0300

    Haciendo que el selector de acompañantes filtre por region de usuario loggeado. Agregando cargo y region a la etiqueta de usuario.

--- app/components/suite-selector/acompaniantes.js
+++ app/components/suite-selector/acompaniantes.js
@@ -3,13 +3,18 @@ import { task, timeout } from "ember-concurrency";
 
 export default Ember.Component.extend({
   store: Ember.inject.service(),
+  perfilService: Ember.inject.service("perfil"),
 
   buscarPersonas: task(function*(term) {
     yield timeout(200);
-    let query = { search: term };
 
-    if (this.get("region")) {
-      query.region__numero = this.get("region");
+    let soloSuRegion = !this.get("perfilService").tienePermiso("perfil.global");
+
+    var query = { search: term };
+
+    if (soloSuRegion) {
+      let region = this.get("perfilService").obtenerRegion();
+      query.region__numero = region.get("numero");
     }
 
     return this.get("store").query("perfil", query);
