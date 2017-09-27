0ec44fa6bfcc2c51ed469c257ad0eb9d3331856f
Iván Hoffmann <digital.h@gmail.com>
2017-09-27 12:25:01 -0300

    Fix en componente de descarga de paquete legacy

--- app/components/suite-detalle/zip-paquete.js
+++ app/components/suite-detalle/zip-paquete.js
@@ -4,10 +4,11 @@ export default Ember.Component.extend({
   linkDescarga: Ember.computed("model.idDevolucion", function() {
     var linkPaquete;
     let valor = this.get("model.idDevolucion");
-    if (valor != 0) {
-      linkPaquete = true;
-    } else {
+    console.log(valor);
+    if (valor == 0 || valor == null) {
       linkPaquete = false;
+    } else {
+      linkPaquete = true;
     }
     return linkPaquete;
   })
