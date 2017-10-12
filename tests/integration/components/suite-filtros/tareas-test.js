c035bfea3bd79c71064e6c4ef0bf9c18a60cb19e
Iván Hoffmann <digital.h@gmail.com>
2017-10-12 16:22:15 -0300

    Implementando filtro por región en lista de tareas.

--- tests/integration/components/suite-filtros/tareas-test.js
+++ tests/integration/components/suite-filtros/tareas-test.js
@@ -10,7 +10,13 @@ moduleForComponent(
 );
 
 test("it renders", function(assert) {
+  this.set("cuandoSeleccionaRegion", function() {});
   this.set("alIngresarFiltro", function() {});
-  this.render(hbs`{{suite-filtros/tareas alIngresarFiltro=alIngresarFiltro}}`);
-  assert.equal(this.$().text().trim(), "Buscar");
+
+  this.render(hbs`{{suite-filtros/tareas
+    cuandoSeleccionaRegion=cuandoSeleccionaRegion
+    alIngresarFiltro=alIngresarFiltro
+  }}`);
+
+  assert.ok(this.$().text());
 });
