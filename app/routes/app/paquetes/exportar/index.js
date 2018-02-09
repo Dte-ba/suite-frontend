349e191e3d9d591afabc6c1c7901d8968c12f526
Iván Hoffmann <digital.h@gmail.com>
2018-02-09 09:25:45 -0300

    Implementando combo de estado en exportador de paquetes

--- app/routes/app/paquetes/exportar/index.js
+++ app/routes/app/paquetes/exportar/index.js
@@ -1,13 +1,22 @@
 import Ember from "ember";
 
 export default Ember.Route.extend({
+  model() {
+    return this.set("opciones", {
+      estadosDePaquete: this.store.findAll("estadoDePaquete")
+    });
+  },
   actions: {
     iniciarExportacion(datos) {
+      var estado = datos.get("estado.nombre");
+      if (!estado) {
+        estado = "Todos";
+      }
       this.transitionTo("app.paquetes.exportar.exportar", {
         queryParams: {
           desde: datos.get("desde"),
           hasta: datos.get("hasta"),
-          estado: "Pendiente"
+          estado: estado
         }
       });
     }
