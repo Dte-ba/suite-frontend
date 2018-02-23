6fe677a7e22ee171ca4234398c540e78beb1deaa
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-02-23 11:06:46 -0300

    Haciendo que todos los formularios muestren un resumen de los errores de validación al final del formulario.

--- app/components/suite-errores-de-formulario.js
+++ app/components/suite-errores-de-formulario.js
@@ -1,10 +1,6 @@
 import Ember from "ember";
 
 export default Ember.Component.extend({
-  errores: Ember.computed.alias("formulario.model.errors"),
-  didInsertElement() {
-    if (!this.get("formulario")) {
-      throw new Error("Tienes que especificar el parámetro formulario.");
-    }
-  }
+  errores: Ember.computed.alias("model.errors"),
+  submitted: Ember.computed.alias("form.submitted")
 });
