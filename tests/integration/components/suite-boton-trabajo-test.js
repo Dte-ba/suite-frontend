9905606ce86986523a06721c1c7f4cbced852572
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-02-26 01:28:14 -0300

    Reparando tests.

--- tests/integration/components/suite-boton-trabajo-test.js
+++ tests/integration/components/suite-boton-trabajo-test.js
@@ -1,25 +1,15 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from "ember-qunit";
+import hbs from "htmlbars-inline-precompile";
 
-moduleForComponent('suite-boton-trabajo', 'Integration | Component | suite boton trabajo', {
+moduleForComponent("suite-boton-trabajo", "Integration | Component | suite boton trabajo", {
   integration: true
 });
 
-test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
+test("it renders", function(assert) {
   this.render(hbs`{{suite-boton-trabajo}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#suite-boton-trabajo}}
-      template block text
-    {{/suite-boton-trabajo}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.ok(
+    this.$()
+      .text()
+      .trim()
+  );
 });
