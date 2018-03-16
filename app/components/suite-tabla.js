0c2b04594d6942be47590e4d2a728527a1d0cd5a
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-03-16 16:39:09 -0300

    Reemplazando el container de semantic-ui por uno propio.

--- app/components/suite-tabla.js
+++ app/components/suite-tabla.js
@@ -1,7 +1,7 @@
 import Ember from "ember";
 
 export default Ember.Component.extend({
-  classNames: ["suite-tabla"],
+  classNames: ["suite-tabla", "dt--fixed"],
   tarea: null,
   filtro: undefined,
   pagina: null,
