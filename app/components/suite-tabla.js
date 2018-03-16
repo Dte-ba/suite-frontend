b2399b0a7c426d0d8c8f5069d7f245ebd6ef5132
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-03-16 17:47:59 -0300

    Corrigiendo tablas para que no excedan el tamaño de pantalla.

--- app/components/suite-tabla.js
+++ app/components/suite-tabla.js
@@ -1,7 +1,7 @@
 import Ember from "ember";
 
 export default Ember.Component.extend({
-  classNames: ["suite-tabla", "dt--fixed"],
+  classNames: ["suite-tabla"],
   tarea: null,
   filtro: undefined,
   pagina: null,
