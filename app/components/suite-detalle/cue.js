c6be102d1150a05cf4b871e1d3de72618063e88f
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-03-16 18:20:00 -0300

    Reemplanzando ui-popup por una versión propia en cue y actas de eventos.

--- app/components/suite-detalle/cue.js
+++ app/components/suite-detalle/cue.js
@@ -1,4 +1,11 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Component.extend({
+  mouseEnter() {
+    this.set("debeMostrarPopup", true);
+  },
+
+  mouseLeave() {
+    this.set("debeMostrarPopup", false);
+  }
 });
