256671b7709f3f2a2d966c0d3166568be3aca1b5
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-03-17 00:38:17 -0300

    Reemplazando el menú de navegación mobile para evitar usar el que trae semantic.

--- app/components/suite-barra-de-navegacion.js
+++ app/components/suite-barra-de-navegacion.js
@@ -1,15 +1,19 @@
 import Ember from "ember";
 
 export default Ember.Component.extend({
-  classNames: ["ui", "fixed", "menu", "inverted", "barra-de-navegacion"],
+  classNames: ["ui", "fixed", "menu", "inverted", "barra-de-navegacion", "z-max"],
   perfil: Ember.inject.service(),
   nombreCompleto: Ember.computed.alias("perfil.nombreCompleto"),
   rol: Ember.computed.alias("perfil.rol"),
   region: Ember.computed.alias("perfil.region"),
+  sidebarVisible: true,
 
   actions: {
-    toggle: function(id) {
-      $(`#${id}`).sidebar("toggle");
+    mostrarSidebar() {
+      this.set("sidebarVisible", true);
+    },
+    ocultarSidebar() {
+      this.set("sidebarVisible", false);
     }
   }
 });
