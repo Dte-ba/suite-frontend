814099c73b6b364e63f5a209a8d101dc419100ee
Iván Hoffmann <digital.h@gmail.com>
2018-04-05 11:31:01 -0300

    Cambios menores en helper de estado de piso

--- app/helpers/estado-de-piso.js
+++ app/helpers/estado-de-piso.js
@@ -5,7 +5,7 @@ export function estadoDeTarea(params, namedArgs/*, hash*/) {
   let label = namedArgs.label;
 
   if (estado === true ) {
-    estado = '<i class="ui green server icon"></i>';
+    estado = '<i class="ui blue server icon"></i>';
     if (label === "True") {
       estado = estado + 'Piso funcionando';
     }
