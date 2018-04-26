356398b30793ec29b0e7b5bc46018157bc6d98dc
Iván Hoffmann <digital.h@gmail.com>
2018-04-26 18:31:17 -0300

    Implementando validacion para que no se pueda dejar Nivel y Modalidad con opcion Sin.

--- app/components/suite-formularios/escuela.js
+++ app/components/suite-formularios/escuela.js
@@ -8,11 +8,28 @@ export default Ember.Component.extend({
       let piso = null;
 
       if (!modelo.get("piso.id")) {
-        piso = yield this.get("store").createRecord("piso", {}).save();
+        piso = yield this.get("store")
+          .createRecord("piso", {})
+          .save();
       }
 
       if (piso) {
         modelo.set("piso", piso);
+      }
+
+      let nivel = modelo.get("nivel");
+      let modalidad = modelo.get("modalidad");
+
+      if (
+        nivel.get("nombre") === "Sin Nivel" &&
+        modalidad.get("nombre") === "Sin Modalidad"
+      ) {
+        this.set(
+          "error",
+          "No se puede guardar una escuela Sin Nivel y Sin Modalidad. Debe indicar alguno de los datos."
+        );
+
+        return;
       }
 
       let escuela = yield modelo.save();
@@ -20,7 +37,6 @@ export default Ember.Component.extend({
       if (this.get("cuandoGuarda")) {
         this.get("cuandoGuarda")();
       }
-
       return escuela;
     } catch (e) {
       throw new Error("Ha ocurrido un error del lado del servidor");
