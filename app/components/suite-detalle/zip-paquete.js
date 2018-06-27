9838ea2ae975e5ceb0541dfec9cf93e3f3fe1c0a
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-06-27 17:08:51 -0300

    Haciendo que cambie de estado un paquete cuando se descarga.

--- app/components/suite-detalle/zip-paquete.js
+++ app/components/suite-detalle/zip-paquete.js
@@ -1,6 +1,11 @@
 import Ember from "ember";
+import ENV from "suite-frontend/config/environment";
 
 export default Ember.Component.extend({
+  descargas: Ember.inject.service(),
+  ajax: Ember.inject.service(),
+  store: Ember.inject.service(),
+
   linkDescarga: Ember.computed("model.idDevolucion", function() {
     var linkPaquete;
     let valor = this.get("model.idDevolucion");
@@ -10,5 +15,20 @@ export default Ember.Component.extend({
       linkPaquete = true;
     }
     return linkPaquete;
-  })
+  }),
+
+  actions: {
+    marcarComoDescargado(paquete) {
+      let base = ENV.API_URL;
+      let url = `${base}/api/paquetes/${paquete.id}/marcar-como-descargado`;
+
+      this.get("descargas").iniciarDesdeURL(paquete.get("zipDevolucion"));
+
+      this.get("ajax")
+        .request(url, { method: "POST" })
+        .then(() => {
+          this.get("store").findRecord("paquete", paquete.get("id"));
+        });
+    }
+  }
 });
